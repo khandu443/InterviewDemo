7ff125b222434b6cafc0d5f22e5d0e7db9c3f595
khandu443 <ravisagar328@gmail.com>
2023-10-12 16:26:36 +0530

    Update ec2.tf

--- Ch-1_3_TierArch/ec2.tf
+++ Ch-1_3_TierArch/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web-1" {
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
-  subnet_id              = aws_subnet.web-subnet-1.id
+  subnet_id              = aws_subnet.pub-sub-1.id
   user_data              = file("install_apache.sh")
 
   tags = {
@@ -18,11 +18,23 @@ resource "aws_instance" "web-2" {
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1b"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
-  subnet_id              = aws_subnet.web-subnet-2.id
+  subnet_id              = aws_subnet.pub-sub-2.id
   user_data              = file("install_apache.sh")
 
   tags = {
     Name = "Web Server 2"
   }
 
+}
+
+resource "aws_instance" "app-srv-1" {
+  ami                    = ami-0d5eff06f840b45e9
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.private-sub-1.id
+  vpc_security_group_ids = [aws_security_group.app-sg.id]
+  key_name               = "source_key"
+
+  tags = {
+    Name = "app-asg"
+  }
 }
