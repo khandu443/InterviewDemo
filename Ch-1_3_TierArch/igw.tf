c06ae18511ff329f9aa587190a3984035d153da1
khandu443 <ravisagar328@gmail.com>
2023-10-12 15:58:07 +0530

    Update igw.tf

--- Ch-1_3_TierArch/igw.tf
+++ Ch-1_3_TierArch/igw.tf
@@ -18,17 +18,17 @@ resource "aws_route_table" "web-rt" {
   }
 
   tags = {
-    Name = "WebRT"
+    Name = "routes"
   }
 }
 
 # Create Web Subnet association with Web route table
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.web-subnet-1.id
+  subnet_id      = aws_subnet.pub-sub-1.id
   route_table_id = aws_route_table.web-rt.id
 }
 
 resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.web-subnet-2.id
+  subnet_id      = aws_subnet.pub-sub-2.id
   route_table_id = aws_route_table.web-rt.id
 }
