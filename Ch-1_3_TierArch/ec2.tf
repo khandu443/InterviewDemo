c8671afe569c4ab473de88e11ae466c457508218
khandu443 <ravisagar328@gmail.com>
2023-10-12 16:44:02 +0530

    Update ec2.tf

--- Ch-1_3_TierArch/ec2.tf
+++ Ch-1_3_TierArch/ec2.tf
@@ -38,3 +38,15 @@ resource "aws_instance" "app-srv-1" {
     Name = "app-asg"
   }
 }
+
+resource "aws_instance" "app-srv-2" {
+  ami                    = ami-0d5eff06f840b45e9
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.private-sub-1.id
+  vpc_security_group_ids = [aws_security_group.app-sg.id]
+  key_name               = "source_key"
+
+  tags = {
+    Name = "app-asg"
+  }
+}
